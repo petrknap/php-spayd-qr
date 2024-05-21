63a8339cb62537ff515ff3bf93a1e091a105b8ff
Tormi Talv <tormi.talv@sportlyzer.com>
2024-05-21 14:19:00 +0300

    Constants and setters for data fields (https://qr-platba.cz/pro-vyvojare/specifikace-formatu/); Basic data sanitation; Getter for Spayd; Getter for underlying QR code builder

--- src/SpaydQr.php
+++ src/SpaydQr.php
@@ -6,8 +6,8 @@ use Endroid\QrCode\Builder\Builder;
 use Endroid\QrCode\Builder\BuilderInterface;
 use Endroid\QrCode\Encoding\Encoding;
 use Endroid\QrCode\Writer\PngWriter;
-use Endroid\QrCode\Writer\WriterInterface;
 use Endroid\QrCode\Writer\Result\ResultInterface;
+use Endroid\QrCode\Writer\WriterInterface;
 use Money\Currencies\ISOCurrencies;
 use Money\Formatter\DecimalMoneyFormatter;
 use Money\Money;
@@ -47,6 +47,65 @@ class SpaydQr implements SpaydQrInterface
         return $this;
     }
 
+    public function setIban(string $iban): self
+    {
+        $this->spayd->add(self::SPAYD_IBAN, $this->sanitizeString($iban, 46));
+        return $this;
+    }
+
+    public function setAltAccount(array $altAccounts): self
+    {
+        $altAccountsJoined = implode(',', array_splice($altAccounts, 0, 2));
+        if (mb_strlen($altAccountsJoined) > 93) {
+            // Take only one
+            $altAccountsJoined = implode(',', array_splice($altAccounts, 0, 1));
+        }
+        $this->spayd->add(self::SPAYD_ALT_ACCOUNT, $altAccountsJoined);
+        return $this;
+    }
+
+    public function setReference(string $reference): self
+    {
+        $this->spayd->add(self::SPAYD_REFERENCE, $this->sanitizeString($reference, 16));
+        return $this;
+    }
+
+    public function setRecipientName(string $recipientName): self
+    {
+        $this->spayd->add(self::SPAYD_RECIPIENT_NAME, $this->sanitizeString($recipientName, 35, true));
+        return $this;
+    }
+
+    public function setDueDate(string $dueDate): self
+    {
+        $this->spayd->add(self::SPAYD_DUE_DATE, $this->sanitizeString($dueDate, 8));
+        return $this;
+    }
+
+    public function setPaymentType(string $paymentType): self
+    {
+        $this->spayd->add(self::SPAYD_PAYMENT_TYPE, $this->sanitizeString($paymentType, 3, true));
+        return $this;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->spayd->add(self::SPAYD_MESSAGE, $this->sanitizeString($message, 60, true));
+        return $this;
+    }
+
+    public function setNotificationType(string $notificationType): self
+    {
+        $this->spayd->add(self::SPAYD_NOTIFICATION_TYPE, $this->sanitizeString($notificationType, 1));
+        return $this;
+    }
+
+    public function setNotification(string $notification): self
+    {
+        $this->spayd->add(self::SPAYD_NOTIFICATION, $this->sanitizeString($notification, 320));
+        return $this;
+    }
+
     public function setInvoice(
         string $id,
         \DateTimeInterface $issueDate,
@@ -59,7 +118,7 @@ class SpaydQr implements SpaydQrInterface
         $normalize = function (string $input): string {
             return str_replace(
                 ['*', '%2A', '%2a'],
-                ['' , ''   , ''   ],
+                ['', '', ''],
                 $input
             );
         };
@@ -107,6 +166,16 @@ class SpaydQr implements SpaydQrInterface
         $this->buildQrCode($size, $margin)->saveToFile($path);
     }
 
+    public function getSpayd(): Spayd
+    {
+        return $this->spayd;
+    }
+
+    public function getQrCodeBuilder(): BuilderInterface
+    {
+        return $this->qrCodeBuilder;
+    }
+
     /** @internal */
     protected function buildQrCode(?int $size, ?int $margin): ResultInterface
     {
@@ -129,5 +198,14 @@ class SpaydQr implements SpaydQrInterface
         $moneyFormatter = new DecimalMoneyFormatter($currencies);
 
         return $moneyFormatter->format($money);
+    }
+
+    private function sanitizeString(string $string, int $maxLength, bool $toUppercase = false): string
+    {
+        if ($toUppercase) {
+            $string = mb_strtoupper($string, 'UTF-8');
+        }
+
+        return mb_strcut($string, 0, $maxLength, 'UTF-8');
     }
 }
--- src/SpaydQrInterface.php
+++ src/SpaydQrInterface.php
@@ -2,14 +2,27 @@
 
 namespace PetrKnap\SpaydQr;
 
+use Endroid\QrCode\Builder\BuilderInterface;
 use Endroid\QrCode\Writer\WriterInterface;
 use Money\Money;
+use Sunfox\Spayd\Spayd;
 
 interface SpaydQrInterface
 {
+    // Specification: https://qr-platba.cz/pro-vyvojare/specifikace-formatu/
+    // Field constants
     public const SPAYD_IBAN = 'ACC';
+    public const SPAYD_ALT_ACCOUNT = 'ALT-ACC';
     public const SPAYD_AMOUNT = 'AM';
     public const SPAYD_CURRENCY = 'CC';
+    public const SPAYD_REFERENCE = 'RF';
+    public const SPAYD_RECIPIENT_NAME = 'RN';
+    public const SPAYD_DUE_DATE = 'DT';
+    public const SPAYD_PAYMENT_TYPE = 'PT';
+    public const SPAYD_MESSAGE = 'MSG';
+    public const SPAYD_NOTIFICATION_TYPE = 'NT';
+    public const SPAYD_NOTIFICATION = 'NO';
+
     public const SPAYD_VARIABLE_SYMBOL = 'X-VS';
     public const SPAYD_INVOICE = 'X-INV';
     public const SPAYD_INVOICE_FORMAT = 'SID';
@@ -27,7 +40,91 @@ interface SpaydQrInterface
 
     public static function create(string $iban, Money $money): self;
 
+    // Specification: https://qr-platba.cz/pro-vyvojare/specifikace-formatu/
+    // Setters
+    /**
+     * Identification of the counterparty, which is composed of two components separated by a + sign.
+     *
+     * @param string $iban
+     * @return self
+     */
+    public function setIban(string $iban): self;
+
+    /**
+     * A list of alternate accounts to the default account given by the ACC value. Individual records have the same format as the ACC field and are separated by a comma.
+     * Max 2.
+     *
+     * @param array $altAccounts
+     * @return self
+     */
+    public function setAltAccount(array $altAccounts): self;
+
+    // Amount and currency set via \Money\Money $money in constructor
+
+    /**
+     * Payment identifier for the payee.
+     *
+     * @param string $reference
+     * @return self
+     */
+    public function setReference(string $reference): self;
+
+    /**
+     * Name of the recipient.
+     *
+     * @param string $recipientName
+     * @return self
+     */
+    public function setRecipientName(string $recipientName): self;
+
+    /**
+     * Due date.
+     *
+     * @param string $dueDate
+     * @return self
+     */
+    public function setDueDate(string $dueDate): self;
+
+    /**
+     * Type of payment.
+     *
+     * @param string $paymentType
+     * @return self
+     */
+    public function setPaymentType(string $paymentType): self;
+
+    /**
+     * Message for the recipient.
+     *
+     * @param string $message
+     * @return self
+     */
+    public function setMessage(string $message): self;
+
+    /**
+     * Identification of the channel for sending the notification to the issuer of the payment.
+     *
+     * @param string $notificationType
+     * @return self
+     */
+    public function setNotificationType(string $notificationType): self;
+
+    /**
+     * Telephone number in international or local terms, or email address.
+     *
+     * @param string $notification
+     * @return self
+     */
+    public function setNotification(string $notification): self;
+
+    /**
+     * Variable symbol. Czech specific reference number.
+     *
+     * @param int $variableSymbol
+     * @return self
+     */
     public function setVariableSymbol(int $variableSymbol): self;
+
     /** @see https://qr-faktura.cz/ */
     public function setInvoice(
         string $id,
@@ -38,11 +135,29 @@ interface SpaydQrInterface
         ?string $buyerVatIdentificationNumber,
         ?string $description
     ): self;
+
     /** @todo create own enum of writers */
     public function setWriter(WriterInterface $writer): self;
 
     public function getContentType(): string;
+
     public function getContent(int $size = self::QR_SIZE, int $margin = self::QR_MARGIN): string;
+
     public function getDataUri(int $size = self::QR_SIZE, int $margin = self::QR_MARGIN): string;
+
     public function writeFile(string $path, int $size = self::QR_SIZE, int $margin = self::QR_MARGIN): void;
+
+    /**
+     * Get the underlying Spayd instance if needed.
+     *
+     * @return Spayd
+     */
+    public function getSpayd(): Spayd;
+
+    /**
+     * Get the underlying QR code builder instance if needed to customize properties like labels, colors, etc.
+     *
+     * @return BuilderInterface
+     */
+    public function getQrCodeBuilder(): BuilderInterface;
 }
